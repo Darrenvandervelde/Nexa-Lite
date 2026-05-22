3a60e69ef2f60d1c69f250eb68a3f0c45a344577
Darren van der Velde <darrenvandervelde@gmail.com>
2026-05-22 15:35:09 +0100

    Import addInvoice function in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useState } from "react";
 import "./App.css";
+import addInvoice from "addInvoice.js";
 
 const AUTH_KEY = "nexa_auth";
 const INVOICE_KEY = "nexa_invoices";
